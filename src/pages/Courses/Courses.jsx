53e49f492ffc5f747a7e75369dc2ef1792872ebe
Jeena01 <jeenajavahar@gmail.com>
2024-03-02 12:30:19 -0800

    Update Courses.jsx

--- src/pages/Courses/Courses.jsx
+++ src/pages/Courses/Courses.jsx
@@ -1,20 +1,31 @@
 import React from 'react'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
 
 function Courses() {
   /**create an array with strings for various subjects like computer, maht, bio etc */
   const subjects = ['Computer', 'Math', 'Biology', 'Physics', 'Chemistry', 'English'];
 
-  /** return the subjects listed in Card react components */
-    return (
-      <div className="courses">
-        {subjects.map((subject, index) => (
-          <div className="card" key={index}>
-            <h2>{subject}</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
-          </div>
+ /**use the array to create card components from material ui */
+
+return(
+    <Box display="flex" alignItems="center"  gap={4} p={2}
+    >
+        {subjects.map(subject => (
+            <Card variant="outlined">
+                <CardContent>
+                 
+                <Typography variant="body2" color="text.secondary">
+                    {subject}
+          </Typography>
+                </CardContent>
+            </Card>
         ))}
-        </div>
-    )
+      </Box>
+)
         
 }
 
