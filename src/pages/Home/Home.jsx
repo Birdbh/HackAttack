1c14855add334175d002456b51954298cffe1517
Jeena01 <jeenajavahar@gmail.com>
2024-03-02 16:50:03 -0800

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Hero from '../../components/Hero/Hero';
 import About from '../../components/About/About';
-import Testimonial from '../../components/Testimonials/Testimonial';
 function Home() {
   return (
     <div>
       <Hero />
       <About />
-      <Testimonial/>
     </div>
   )
 }
